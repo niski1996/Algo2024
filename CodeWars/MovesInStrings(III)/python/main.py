124eedea9825b810b0b9f338209b797205892e36
niski1996 <niski1996@interia.pl>
2025-01-19 00:19:56 +0100

    Autogenerated commit

---
+++ CodeWars/MovesInStrings(III)/python/main.py
@@ -0,0 +1,19 @@
+def diag_1_sym(strng):
+    letterMatrix = getLetterMatrix(strng)
+    outputMatrix = getEmptyLetterMatrix(strng)
+    for rnum, row in enumerate(letterMatrix):
+        for cnum, column in enumerate(row):
+            outputMatrix[cnum][rnum]=letterMatrix[rnum][cnum]
+
+
+def getLetterMatrix(strng):
+    return [[letter for letter in row] for row in strng.split("\n")]
+
+def getEmptyLetterMatrix(strng):
+    return [["" for letter in row] for row in strng.split("\n")]
+
+
+
+s = "abcd\nefgh\nijkl\nmnop"
+print(s)
+print(diag_1_sym(s))
